7f895262778fd5cf9ff8b51ad743541fe6853f44
Adilyam Tilegenova <adilyamt@gmail.com>
2020-07-25 01:33:22 +0600

    Update bubble_sort.py

--- bubble-sort/bubble_sort.py
+++ bubble-sort/bubble_sort.py
@@ -1,2 +1,8 @@
 def bubble_sort(lst):
+  for i in range(len(lst)):
+    for j in range(1, len(lst)):
+      if lst[j] < lst[j - 1]:
+        temp = lst[j]
+        lst[j] = lst[j - 1]
+        lst[j - 1] = temp
     # sort the list via bubble sort
